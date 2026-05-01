7cab37b3753037f46d9492cc4ffedcd2d96776bf
Code-Yves <Yves@blueprintbuilds.com>
2026-05-01 14:40:53 -0400

    Delete _design-history directory

--- _design-history/Original/serve.py
+++
@@ -1,5 +0,0 @@
-import http.server, socketserver, os
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
-handler = http.server.SimpleHTTPRequestHandler
-with socketserver.TCPServer(("", 8091), handler) as httpd:
-    httpd.serve_forever()
